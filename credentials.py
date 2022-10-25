6b8bee3550574fef41c1ebfe3f8206b649fd3b26
Daniel <83504117+ELODollZ@users.noreply.github.com>
2022-10-25 11:55:41 +0200

    credentials

---
+++ credentials.py
@@ -0,0 +1,9 @@
+credentials = {
+    'ssid' : 'dani636e',
+    'password' : 'dani636e',
+    'ADAFRUIT_IO_URL' : b'io.adafruit.com',
+    'ADAFRUIT_USERNAME' : b'dani636e',
+    'ADAFRUIT_IO_KEY' : b'aio_PZhH92pL9th2HRUYLhZs4KTNYmVU',
+    'ADAFRUIT_IO_FEEDNAME' : b'IOTFeed'
+    }
+
