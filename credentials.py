9c2da7ab614f54591dc88197289cb1443dcf63e1
Kasperfcb <kltn2000@hotmail.com>
2022-10-25 12:09:29 +0200

    t

--- credentials.py
+++ credentials.py
@@ -1,9 +1,9 @@
 credentials = {
-    'ssid' : 'dani636e',
-    'password' : 'dani636e',
+    'ssid' : 'Skoletest',
+    'password' : '1234567891',
     'ADAFRUIT_IO_URL' : b'io.adafruit.com',
-    'ADAFRUIT_USERNAME' : b'dani636e',
-    'ADAFRUIT_IO_KEY' : b'aio_PZhH92pL9th2HRUYLhZs4KTNYmVU',
-    'ADAFRUIT_IO_FEEDNAME' : b'IOTFeed'
+    'ADAFRUIT_USERNAME' : b'Kasperfcb',
+    'ADAFRUIT_IO_KEY' : b'aio_SbLP93dF2CLwutHY9Wdz3Hh11ny6',
+    'ADAFRUIT_IO_FEEDNAME' : b'mapfeed'
     }
 
