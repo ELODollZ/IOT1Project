928e381d440e98fa481dcdc06d496452c0b196c7
ELODollZ <nybodaniel@gmail.com>
2022-11-04 09:34:54 +0100

    small update
    cleaned code

--- Display4X7.py
+++ Display4X7.py
@@ -7,14 +7,14 @@ tm = tm1637.TM1637(clk=Pin(4), dio=Pin(2))
 
 tm.brightness(5)
 variableY = "LIVE"
-
+sleep(1)
+tm.show("PPPP")
 def display():
     variableX = Batteryreader.battery_percentage
     variableX = int(variableX)
     #tm.show(variableY)
-    sleep(1)
-    tm.show("PPPP")
-    tm.show(("{}{}".format(variableX,"P")))
     if(variableX < 100):
         tm.show(("{}{}".format("0", variableX, "P")))
+    else:
+        tm.show(("{}{}".format(variableX,"P")))
     
